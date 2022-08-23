d8ca2b3925d5453dd8de9212bec7034618559cf0
pastisx1 <109997871+pastisx1@users.noreply.github.com>
2022-08-23 12:37:12 -0300

    cambios 23/08

---
+++ Controllers/Dto/PostVenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prefinalentrega1.Dto
+{
+    public class PostVenta
+    {
+        public int IdUsuario { get; set; }
+        public int IdProducto { get; set; }
+        public int Stock { get; set; }
+        public string Status { get; set; }
+    }
+}
